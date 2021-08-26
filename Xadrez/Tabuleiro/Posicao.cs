74806c1903d1c163a2200afe446af2919c4cb679
Bryan <bryanlima200@outlook.com>
2021-08-25 23:32:06 -0300

    Implementação inicial da classe Posicao

--- Xadrez/Tabuleiro/Posicao.cs
+++ Xadrez/Tabuleiro/Posicao.cs
@@ -2,9 +2,23 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Xadrez.Tabuleiro
+namespace Tabuleiro
 {
     class Posicao
     {
+        public int Linha { get; set; }
+
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            this.Linha = linha;
+            this.Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return $"{Linha}, {Coluna}";
+        }
     }
 }
