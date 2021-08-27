d7db74e2d6b064a9ae5a4513b38de296bba07a70
Bryan <bryanlima200@outlook.com>
2021-08-26 22:46:54 -0300

    Implementação inicial da classe Peca

--- Xadrez/Tabuleiro/Peca.cs
+++ Xadrez/Tabuleiro/Peca.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Xadrez.Tabuleiro
+﻿namespace Tabuleiro
 {
     class Peca
     {
+        public Posicao Posicao { get; set; }
+
+        public Cor Cor { get; protected set; }
+
+        public int QuantidadeMovimentos { get; protected set; }
+
+        public TabuleiroXadrez Tabuleiro { get; set; }
+
+        public Peca(Posicao posicao, TabuleiroXadrez tabuleiro, Cor cor)
+        {
+            this.Posicao = posicao;
+            this.Tabuleiro = tabuleiro;
+            this.Cor = cor;
+            this.QuantidadeMovimentos = 0;
+        }
     }
 }
