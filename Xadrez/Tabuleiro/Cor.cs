9bbfbf0ac84f45c4a2b17c9e568dd0d76ba9ca31
Bryan <bryanlima200@outlook.com>
2021-08-26 22:46:28 -0300

    Implementado enumerador Cor

--- Xadrez/Tabuleiro/Cor.cs
+++ Xadrez/Tabuleiro/Cor.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Xadrez.Tabuleiro
+﻿namespace Tabuleiro
 {
-    class Cor
+    enum Cor
     {
+        Branca,
+        Preta
     }
 }
