e700e1c83f44c5ecb706705d0b86b1f4f83b9239
Bryan <bryanlima200@outlook.com>
2021-08-26 22:45:54 -0300

    Renomeado classe Tabuleiro para TabuleiroXadrez Devido namespace já ser Tabuleiro, há conflito ao tentar usar a classe diretamente

---
+++ Xadrez/Tabuleiro/TabuleiroXadrez.cs
@@ -0,0 +1,18 @@
+﻿namespace Tabuleiro
+{
+    class TabuleiroXadrez
+    {
+        public int Linhas { get; set; }
+
+        public int Colunas { get; set; }
+
+        private Peca[,] Pecas;
+
+        public TabuleiroXadrez(int linhas, int colunas)
+        {
+            this.Linhas = linhas;
+            this.Colunas = colunas;
+            Pecas = new Peca[linhas, colunas];
+        }
+    }
+}
