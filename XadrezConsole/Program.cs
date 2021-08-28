66ab3106d9da924e9cc95ff625567b9d9d7cfafd
Bryan <bryanlima200@outlook.com>
2021-08-27 23:02:06 -0300

    Implementação inicial da classe PosicaoXadrez

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -8,27 +8,11 @@ namespace XadrezConsole
     {
         static void Main(string[] args)
         {
-            try { 
-            TabuleiroXadrez tabuleiro = new TabuleiroXadrez(8, 8);
+            PosicaoXadrez posicaoXadrez = new PosicaoXadrez('a', 1);
 
-            tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
-            tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 9));
-            tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 2));
-            //tabuleiro.ColocarPeca(???, new Posicao(6, 4));
-            //tabuleiro.ColocarPeca(???, new Posicao(7, 0));
-            //tabuleiro.ColocarPeca(???, new Posicao(7, 5));
-            //tabuleiro.ColocarPeca(???, new Posicao(7, 7));
+            Console.WriteLine(posicaoXadrez);
 
-            Tela.ImprimirTabuleiro(tabuleiro);
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(posicaoXadrez.ToPosicao());
 
             Console.ReadLine();
         }
--- XadrezConsole/Xadrez/PosicaoXadrez.cs
+++ XadrezConsole/Xadrez/PosicaoXadrez.cs
@@ -1,10 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Tabuleiro;
 
-namespace XadrezConsole.Xadrez
+namespace Xadrez
 {
     class PosicaoXadrez
     {
+        public char Coluna { get; set; }
+
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.Linha = linha;
+            this.Coluna = coluna;
+        }
+
+        public Posicao ToPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
     }
 }
